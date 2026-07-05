e6adcdbf42f0703b8422530d006c1d6ad805e585
Portinho <m.porto2508@gmail.com>
2026-07-05 12:21:47 -0300

    11 and 12 create a simple web server and routing

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
 const fs = require('fs');
+const http = require('http');
 /*
+/////////////////////////////// FILES
 const textIn = fs.readFileSync('./txt/input.txt', 'utf8');
 console.log(textIn);
 
 const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written!!');*/
+console.log('File written!!');
 
 //non-blocking, asynchronous way to read and write files
 fs.readFile('./txt/start.txt', 'utf8', (err, data1) => {
     if (err) return console.log('ERROR! 💥');
-  fs.readFile(`./txt/${data1}.txt`, 'utf8', (err, data2) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf8', (err, data2) => {
     console.log(data2);
     fs.readFile('./txt/append.txt', 'utf8', (err, data3) => {
       console.log(data3);
-
+      
       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf8', (err) => {
         console.log('Your file has been written');
-      });
     });
-  });
+});
+});
 });
 
 console.log('Will read file!');
+////////////////////////////////////////
+*/
+/////////////////////////////// SERVER
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW');
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT');
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+});
